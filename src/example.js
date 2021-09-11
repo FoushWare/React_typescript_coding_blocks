7b15fa20e54f9e98affd65c457dcd4d1cc65f743
foushware <mr.fouad992@gmail.com>
2021-09-11 03:11:46 +0200

    🏆 Added lint script and ignore files in the production dist folder and other files that is ignored like node-modules

--- src/example.js
+++ src/example.js
@@ -1,12 +1,17 @@
+// eslint-disable-next-line strict
 'use strict'
 
 const username = 'freddy'
+// eslint-disable-next-line valid-typeof
 typeof username === 'strng'
 
+// eslint-disable-next-line no-unsafe-negation
 if (!'serviceWorker' in navigator) {
 	// you have an old browser :-(
 }
 
+// eslint-disable-next-line no-unused-vars
 const greeting = 'hello'
 console.log('${greeting} world!')
+// eslint-disable-next-line no-unexpected-multiline
 [1, 2, 3].forEach(x => console.log(x))
