1d9baa9d0b3eaad073ec4a437b51acfd0248ec28
foushware <mr.fouad992@gmail.com>
2021-09-21 01:51:24 +0200

    coverage threthold..what percentage of coverage to met

--- jest.config.js
+++ jest.config.js
@@ -17,4 +17,20 @@ module.exports = {
   collectCoverageFrom: [
     '**/src/**/*.js',
   ] /** include all the project files in coverage not only the test files */,
+  coverageThreshold: {
+    global: {
+      statements: 31,
+      branches: 18,
+      functions: 29,
+      lines: 29,
+    },
+    //When makeing specific rules for files respect to coverage .. it subtract it's percenge from the global setting Above
+    './src/shared/utils.js': {
+      // make this file strick for coverage ..if anyone change function or add function make sure every line in covered in testing
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
