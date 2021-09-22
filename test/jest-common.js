9901847eb0732bdec68eb4d6708e23f0e6ac161d
foushware <mr.fouad992@gmail.com>
2021-09-22 02:36:20 +0200

    watch typeahead to autocomplete when using Regex pattern and select specific file to test

--- test/jest-common.js
+++ test/jest-common.js
@@ -12,5 +12,9 @@ module.exports = {
     'shared',
     __dirname,
   ],
-  watchPlugins: ['jest-watch-select-projects'],
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
