adc23c6f7ebd5992b53c2e5753df11a90819805e
foushware <mr.fouad992@gmail.com>
2021-09-26 05:18:30 +0200

    Rerender Component with different args

--- src/__tests__/state-user-event.js
+++ src/__tests__/state-user-event.js
@@ -5,10 +5,15 @@ import {FavoriteNumber} from '../favorite-number'
 import user from '@testing-library/user-event'
 
 test('enter an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole} = render(<FavoriteNumber />)
+  const {getByLabelText, getByRole, rerender, debug} = render(
+    <FavoriteNumber />,
+  )
   const input = getByLabelText(/favorite number/i)
   //#### Hey user type 10 to input :)
   user.type(input, '10')
   // fireEvent.change(input, { target: { value: '10' } })
   expect(getByRole('alert')).toHaveTextContent(/The number is invalid/i)
+  debug()
+  rerender(<FavoriteNumber max={10} />)
+  debug()
 })
