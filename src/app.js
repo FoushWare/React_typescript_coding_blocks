c9f06c047b8d3f7702cb8aef03a9aae13364cb53
foushware <mr.fouad992@gmail.com>
2021-10-04 12:00:58 +0200

    deleting unnecessary files

--- src/app.js
+++ src/app.js
@@ -1,17 +1,7 @@
-import Calculator from './calculator'
-
 function App() {
   return (
     <div>
-      <Calculator />
-      <div style={{marginTop: 30, textAlign: 'center'}}>
-        Calculator component{' '}
-        <a href="https://codepen.io/mjijackson/pen/xOzyGX">created</a>
-        {' by '}
-        <br />
-        <a href="https://twitter.com/mjackson">Michael Jackson</a> of{' '}
-        <a href="https://reacttraining.com/">React Training</a>
-      </div>
+      <h1> 🗼Bienvenue par Ahmed fouad [foushware]🇫🇷 </h1>
     </div>
   )
 }
