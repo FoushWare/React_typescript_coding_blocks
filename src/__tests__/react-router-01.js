fb8c7d30730ab66e686fe20f73ea19d23ec9b99d
foushware <mr.fouad992@gmail.com>
2021-10-06 19:11:30 +0200

    custom ui render for component with router-dom BrowserRouter

--- src/__tests__/react-router-01.js
+++ src/__tests__/react-router-01.js
@@ -1,15 +1,24 @@
-import {render, screen} from '@testing-library/react'
+/* eslint-disable react/prop-types */
+import {render as rtlRender, screen} from '@testing-library/react'
 import {BrowserRouter} from 'react-router-dom'
 import userEvent from '@testing-library/user-event'
 import {Main} from '../main'
 
+function render(ui, {route = '/', ...renderOptions} = {}) {
+  window.history.pushState({}, 'Test page', route)
+
+  function Wrapper({children}) {
+    return <BrowserRouter>{children}</BrowserRouter>
+  }
+
+  return rtlRender(ui, {
+    wrapper: Wrapper,
+    ...renderOptions,
+  })
+}
+
 test('main renders about and home and I can naviage to those pages', () => {
-  window.history.pushState({}, 'Test page', '/')
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  )
+  render(<Main />)
 
   expect(screen.getByRole('heading')).toHaveTextContent(/home/i)
   userEvent.click(screen.getByText(/about/i))
@@ -17,13 +26,9 @@ test('main renders about and home and I can naviage to those pages', () => {
 })
 
 test('landing on a bad page shows no match component', () => {
-  window.history.pushState({}, 'Test page', '/something-that-does-not-match')
-
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  )
+  render(<Main />, {
+    route: '/something-that-does-not-match',
+  })
 
   expect(screen.getByRole('heading')).toHaveTextContent(/404/i)
 })
