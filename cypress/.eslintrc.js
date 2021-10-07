fc7c43c2a69cfe50f2b31397e7c4565abf5920da
foushware <mr.fouad992@gmail.com>
2021-10-07 11:37:38 +0200

    install cypress and add support in eslint and .gitignore

--- cypress/.eslintrc.js
+++ cypress/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
-  plugins: ['cypress', 'eslint-plugin-cypress'],
+  // plugins: ['cypress', 'eslint-plugin-cypress'],
+  plugins: ['eslint-plugin-cypress'],
   extends: ['plugin:cypress/recommended'],
   env: {'cypress/globals': true},
 }
