3b5bd7811b55076fcfe3a91c481e09e85e2cbdb2
foushware <mr.fouad992@gmail.com>
2021-10-07 05:54:08 +0200

    redux render compnent with our custom store state

--- src/__tests__/redux-01.js
+++ src/__tests__/redux-01.js
@@ -1,15 +1,32 @@
 import {Counter} from '../redux-counter'
-import {store} from '../redux-store'
+import {store as appStore} from '../redux-store'
 import {Provider} from 'react-redux'
 import userEvent from '@testing-library/user-event'
 import {render, screen} from '@testing-library/react'
+
+import {createStore} from 'redux'
+import {reducer} from '../redux-reducer'
 test('can render with redux with defaults', () => {
   render(
-    <Provider store={store}>
+    <Provider store={appStore}>
       <Counter />
     </Provider>,
   )
 
   userEvent.click(screen.getByText('+'))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
+})
+
+test('can render with redux with custom intial state ', () => {
+  //We can depend on our store
+  const store = createStore(reducer, {count: 3})
+
+  render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>,
+  )
+
+  userEvent.click(screen.getByText('-'))
+  expect(screen.getByLabelText(/count/i)).toHaveTextContent('2')
 })
